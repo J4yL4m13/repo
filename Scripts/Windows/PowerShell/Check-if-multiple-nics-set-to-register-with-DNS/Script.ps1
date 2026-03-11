071b0a7f2c771def4e6939d6b4962b1ded25a76f
James Lambert <lambertj13@gmail.com>
2026-03-11 18:00:27 +0000

    initial

---
+++ Scripts/Windows/PowerShell/Check-if-multiple-nics-set-to-register-with-DNS/Script.ps1
@@ -0,0 +1,14 @@
+cls
+$servers = Get-Content computers.txt
+
+foreach($server in $servers)
+
+{
+
+ $nics = Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $server -ErrorAction Inquire | Where{$_.IPEnabled -eq "TRUE"}
+
+	foreach($nic in $nics)
+	{
+	Write-Host $server "-" $nic.IPAddress "-" $nic.FullDNSRegistrationEnabled
+	}
+}
